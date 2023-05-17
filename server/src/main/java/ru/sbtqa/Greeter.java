10ea24ec7f865d636ba6aced05e421c8632ca3b8
sergey m <msmsamara@mail.ru>
2023-05-17 11:39:06 +0300

    handle warnings

--- server/src/main/java/ru/sbtqa/Greeter.java
+++ server/src/main/java/ru/sbtqa/Greeter.java
@@ -6,15 +6,17 @@ package ru.sbtqa;
 public class Greeter {
 
   /**
-   * Constructor of Greeter class
+   * Constructor of Greeter class.
    */
   public Greeter() {
 
   }
 
-
-
-
+  /**
+   * method for greeting a person
+   * @param name = name of a person
+   * @return = greeting
+   */
   public final String greet(final String name) {
     return String.format("Hello, %s!", name);
   }
